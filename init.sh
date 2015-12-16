fcc17fde3083460e6363e9ba2f62072a45460290
Martin Grund <mgrund@cloudera.com>
2015-12-15 16:29:27 -0800

    Removing static-libstdc++

--- init.sh
+++ init.sh
@@ -137,7 +137,7 @@ if [[ $SYSTEM_GCC -eq 0 ]]; then
 
   FULL_LPATH="-L$BUILD_DIR/gcc-$GCC_VERSION/lib64"
   LDFLAGS="$ARCH_FLAGS $FULL_RPATH $FULL_LPATH"
-  CXXFLAGS="$ARCH_FLAGS -static-libstdc++ -fPIC -O3 -m64"
+  CXXFLAGS="$ARCH_FLAGS -fPIC -O3 -m64"
 else
   CXX="g++ -stdlib=libstdc++"
   LDFLAGS="$ARCH_FLAGS"
